2d928e81729a5d8953c766108ba0d98d08828014
MdAkramKhanJehad <46520536+MdAkramKhanJehad@users.noreply.github.com>
2020-02-17 12:45:41 +0600

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,30 +1,82 @@
 from PIL import Image
 import pickle, pprint
 
-pkl_file = open("trained.pkl", 'rb')
 
-trained = pickle.load(pkl_file)
+# pkl_file = open("trained2.pkl", 'rb')
+# trained = pickle.load(pkl_file)
+# pkl_file.close()
 
-pkl_file.close()
+def calculateAccuracy(index):
+    right = 0
+    wrong = 0
+    avg = 0
+    for i in range(len(index)):
+        if index[i] < 10:
+            img = "newMask/000" + str(index[i]) + ".png"
+        elif index[i] < 100:
+            img = "newMask/00" + str(index[i]) + ".png"
+        else:
+            img = "newMask/0" + str(index[i]) + ".png"
+
+        if index[i] < 10:
+            img2 = "ibtd/Mask/000" + str(index[i]) + ".bmp"
+        elif index[i] < 100:
+            img2 = "ibtd/Mask/00" + str(index[i]) + ".bmp"
+        else:
+            img2 = "ibtd/Mask/0" + str(index[i]) + ".bmp"
+
+        im = Image.open(img, 'r')
+        width, height = im.size
+        pixel_values = list(im.getdata())
+        totalSize = len(pixel_values)
+
+        im2 = Image.open(img2, 'r')
+        width2, height2 = im2.size
+        pixel_values2 = list(im2.getdata())
+
+        for x in range(totalSize):
+            if pixel_values[x] != pixel_values2[x]:
+                wrong += 1
+            else:
+                right += 1
+
+    avg = (right / (right + wrong)) * 100
+    print('Acuracy : ', avg, '%')
+    return avg
 
 
-testImg = Image.open("zuck.jpg")
-pix = testImg.load()
-testWidth, testHeight = testImg.size
-#print("test width: ", testWidth, " testhei: ", testHeight)
+def main(trained, index):
+    for i in range(len(index)):
+        if index[i] < 10:
+            img = "ibtd/000" + str(index[i]) + ".jpg"
+        elif index[i] < 100:
+            img = "ibtd/00" + str(index[i]) + ".jpg"
+        else:
+            img = "ibtd/0" + str(index[i]) + ".jpg"
 
-for p in range(testWidth-1):
-    for q in range(testHeight-1):
-        r = pix[p,q][0]
-        g = pix[p,q][1]
-        b = pix[p,q][2]
+        testImg = Image.open(img)
+        pix = testImg.load()
+        testWidth, testHeight = testImg.size
+        # print("test width: ", testWidth, " testhei: ", testHeight)
 
-        if trained[r][g][b]<0.4999 :
-            pix[p,q]=(255,255,255)
-            #pix[p,q][1]=255
-            #pix[p,q][2]=255
+        for p in range(testWidth - 1):
+            for q in range(testHeight - 1):
+                r = pix[p, q][0]
+                g = pix[p, q][1]
+                b = pix[p, q][2]
 
-testImg.save("zuck.png")
-print("image saved")
+                if trained[r][g][b] < 0.5555:
+                    pix[p, q] = (255, 255, 255)
+                    # pix[p,q][1]=255
+                    # pix[p,q][2]=255
 
-
+        if index[i] < 10:
+            img2 = "newMask/000" + str(index[i]) + ".png"
+        elif index[i] < 100:
+            img2 = "newMask/00" + str(index[i]) + ".png"
+        else:
+            img2 = "newMask/0" + str(index[i]) + ".png"
+        testImg.save(img2)
+    avrg  = calculateAccuracy(index)
+    return avrg
+# print("image saved")
--- Train.py
+++ Train.py
@@ -1,41 +1,44 @@
+import math
 from PIL import Image
 import pickle
-import os, os.path
+import random
+import Test as test
 
 
 def countPixels(height, width, pixel_values, pixel_values2, skin2, nonSkin2):
-    for count in range(height*width-1):
+    for count in range(height * width - 1):
         a = pixel_values[count][0]
         b = pixel_values[count][1]
         c = pixel_values[count][2]
 
         if pixel_values2[count][0] > 241 and pixel_values2[count][1] > 241 and pixel_values2[count][2] > 241:
-            nonSkin2[a][b][c] +=1
+            nonSkin2[a][b][c] += 1
         else:
-            skin2[a][b][c] +=1
+            skin2[a][b][c] += 1
 
 
-def readImage( skin2, nonSkin2):
+def readImage(skin2, nonSkin2, skipIndex):
     for i in range(555):
-        print("processing image : " ,i)
-
-        if(i<10):
-            img="ibtd/000"+str(i)+".jpg"
-        elif i<100:
-            img="ibtd/00"+str(i)+".jpg"
+        if i in skipIndex:
+            continue
+        print('PROCESSING: ', i)
+        if i < 10:
+            img = "ibtd/000" + str(i) + ".jpg"
+        elif i < 100:
+            img = "ibtd/00" + str(i) + ".jpg"
         else:
-            img="ibtd/0"+str(i)+".jpg"
+            img = "ibtd/0" + str(i) + ".jpg"
 
         im = Image.open(img, 'r')
         width, height = im.size
         pixel_values = list(im.getdata())
 
-        if(i<10):
-            img2="ibtd/Mask/000"+str(i)+".bmp"
-        elif i<100:
-            img2="ibtd/Mask/00"+str(i)+".bmp"
+        if (i < 10):
+            img2 = "ibtd/Mask/000" + str(i) + ".bmp"
+        elif i < 100:
+            img2 = "ibtd/Mask/00" + str(i) + ".bmp"
         else:
-            img2="ibtd/Mask/0"+str(i)+".bmp"
+            img2 = "ibtd/Mask/0" + str(i) + ".bmp"
 
         im2 = Image.open(img2, 'r')
         width2, height2 = im2.size
@@ -49,42 +52,64 @@ def calcSumAndProbability(arr):
     for p in range(256):
         for q in range(256):
             for r in range(256):
-                sum = sum+arr[p][q][r]
+                sum = sum + arr[p][q][r]
 
     for p in range(256):
         for q in range(256):
             for r in range(256):
-                if(arr[p][q][r]!=0):
-                    arr[p][q][r] = arr[p][q][r]/sum
+                if (arr[p][q][r] != 0):
+                    arr[p][q][r] = arr[p][q][r] / sum
 
 
 def calcTrainedData(skin2, nonSkin2, trained2):
-
     for p in range(256):
         for q in range(256):
             for r in range(256):
-                if nonSkin2[p][q][r] ==0 and skin2[p][q][r]!=0:
+                if nonSkin2[p][q][r] == 0 and skin2[p][q][r] != 0:
                     trained2[p][q][r] = 10
-                elif skin2[p][q][r]==0 and nonSkin2[p][q][r] !=0 :
+                elif skin2[p][q][r] == 0 and nonSkin2[p][q][r] != 0:
                     trained2[p][q][r] = 0.000001
-                elif skin2[p][q][r]== 0 and nonSkin2[p][q][r] ==0 :
+                elif skin2[p][q][r] == 0 and nonSkin2[p][q][r] == 0:
                     trained2[p][q][r] = 0.00001
                 else:
-                    trained2[p][q][r] = skin2[p][q][r]/nonSkin2[p][q][r]
+                    trained2[p][q][r] = skin2[p][q][r] / nonSkin2[p][q][r]
 
 
-skin = [[[0 for col in range(256)]for row in range(256)] for x in range(256)]
-nonSkin = [[[0 for col2 in range(256)]for row2 in range(256)] for x2 in range(256)]
-trained = [[[0 for col3 in range(256)]for row3 in range(256)] for x3 in range(256)]
+skin = [[[0 for col in range(256)] for row in range(256)] for x in range(256)]
+nonSkin = [[[0 for col2 in range(256)] for row2 in range(256)] for x2 in range(256)]
+trained = [[[0 for col3 in range(256)] for row3 in range(256)] for x3 in range(256)]
+tempRandomIndex = random.sample(range(0, 555), 555)
 
+indexCounter = 0
+randomIndex = []
+for i in range(10):
+    randomIndex.append([])
+counter = 0
+for i in range(10):
+    for j in range(math.ceil(555 / 10)):
+        randomIndex[i].append(tempRandomIndex[counter])
+        counter += 1
+        if counter == 555:
+            break
 
-readImage(skin, nonSkin)
-calcSumAndProbability(skin)
-calcSumAndProbability(nonSkin)
-calcTrainedData(skin, nonSkin, trained)
+avg = 0
+for i in range(10):
+    readImage(skin, nonSkin, randomIndex[i])
+    calcSumAndProbability(skin)
+    calcSumAndProbability(nonSkin)
+    calcTrainedData(skin, nonSkin, trained)
 
-output = open('trained.pkl', 'wb')
-pickle.dump(trained, output)
-output.close()
+    skin = [[[0 for col in range(256)] for row in range(256)] for x in range(256)]
+    nonSkin = [[[0 for col2 in range(256)] for row2 in range(256)] for x2 in range(256)]
+    trained = [[[0 for col3 in range(256)] for row3 in range(256)] for x3 in range(256)]
 
-print("written in file")
+    avg += test.main(trained, randomIndex[i])
+
+print('\n\nFinal::: ', avg/1,'%')
+
+#
+# output = open('trainedTemp.pkl', 'wb')
+# pickle.dump(trained, output)
+# output.close()
+#
+# print("written in file")
